b348a941fce40c2e702bc2e3d475e8b4ae2ac3d2
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-07 18:35:37 +0200

    Add add-term

---
+++ add-term.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import uuid
+
+print("Term?")
+term = input()
+print("Definition?")
+definition = input()
+
+card_uuid = uuid.uuid1()
+
+with open('/home/prvak/repos/anki-decks/notes/terms.yaml', 'a') as f:
+    f.write("""\t# Added by add-term.py
+\t-
+\t\tuuid: %(uuid)s
+\t\tfront: "%(term)s"
+\t\tback: "%(definition)s"
+\t\tinclude_reverse: true
+""" % {'uuid': card_uuid, 'term': term, 'definition': definition})
+
+print("Term added.")
