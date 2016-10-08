8bcfcd47bcaacc1cadf8b2eca36efa9341e290d7
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-08 12:09:49 +0200

    Fix add-term's indentation

--- add-term.py
+++ add-term.py
@@ -10,12 +10,12 @@ definition = input()
 card_uuid = uuid.uuid1()
 
 with open('/home/prvak/repos/anki-decks/notes/terms.yaml', 'a') as f:
-    f.write("""\t# Added by add-term.py
+    f.write(("""\t# Added by add-term.py
 \t-
 \t\tuuid: %(uuid)s
 \t\tfront: "%(term)s"
 \t\tback: "%(definition)s"
 \t\tinclude_reverse: true
-""" % {'uuid': card_uuid, 'term': term, 'definition': definition})
+""" % {'uuid': card_uuid, 'term': term, 'definition': definition}).replace('\t', ' ' * 8))
 
 print("Term added.")
