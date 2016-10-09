5a45ab45aa0bebde971d9c2762bc1a108fd29a04
Michal Pokorný <prvak.cnx+git@gmail.com>
2016-10-09 21:58:16 +0200

    Add pre-commit hook

---
+++ pre-commit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sys
+import os
+import fnmatch
+import yaml
+
+yaml_files = []
+for root, dirnames, filenames in os.walk('notes'):
+    for filename in fnmatch.filter(filenames, '*.yaml'):
+        yaml_files.append(os.path.join(root, filename))
+
+uuids = set()
+for path in yaml_files:
+    # print(path)
+    with open(path) as yf:
+        try:
+            data = yaml.load(yf)
+        except:
+            print('Invalid YAML file:', path)
+            raise
+
+        for note in data['notes']:
+            uuid = note['uuid']
+            if uuid in uuids:
+                print('Duplicated UUID:', uuid)
+                sys.exit(1)
+            else:
+                uuids.add(uuid)
