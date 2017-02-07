e34555754d7f5d648538562335262bc755cf1709
Michal Pokorný <prvak.cnx+git@gmail.com>
2017-02-07 14:40:10 +0100

    Add tools and refactor common code

---
+++ cards.py
@@ -0,0 +1,124 @@
+import uuid
+import subprocess
+import fnmatch
+import os
+import yaml
+import markdown
+
+def get_git_revision():
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+
+class Note(object):
+    def __init__(self, origin_file, uuid, include_reverse,
+                 topic,
+                 front, back,
+                 git_revision,
+                 deck):
+        self.deck = deck
+        self.origin_file = origin_file
+        self.uuid = uuid
+        self.include_reverse = include_reverse
+        self.topic = topic
+        self.front = front
+        self.back = back
+        self.git_revision = git_revision
+
+    def to_fields(self):
+        return [
+            self.uuid,
+            self.topic,
+            self.front, self.back,
+            'true' if self.include_reverse else '',
+            self.origin_file,
+            self.git_revision,
+
+            #'' # TAGS  --  can add those.
+        ]
+
+def load_all_notes():
+    git_revision = get_git_revision()
+    print 'git rev:', git_revision
+
+    uuids = set()
+
+    my_notes = []
+
+    yaml_files = []
+    # TODO: follow symlinks
+    for root, dirnames, filenames in os.walk('notes'):
+        for filename in fnmatch.filter(filenames, '*.yaml'):
+            yaml_files.append(os.path.join(root, filename))
+
+    for path in yaml_files:
+        print(path)
+        with open(path) as yf:
+            data = yaml.load(yf)
+
+        # if 'uid_tag' in data:
+        #     uid_tag = data['uid_tag'] + '/'
+        # else:
+        #     uid_tag = ''
+
+        if 'topic' in data:
+            topic = data['topic']
+        else:
+            topic = ''
+
+        deck = data['deck']
+
+        for note_row in data['notes']:
+            if not note_row:
+                print(path)
+                print("no note")
+                sys.exit(1)
+            if 'uuid' not in note_row:
+                print(note_row)
+                print('No UUID')
+                sys.exit(1)
+            if 'front' not in note_row or 'back' not in note_row:
+                print(note_row)
+                print('Missing front or back')
+                sys.exit(1)
+
+            note_uuid = note_row['uuid']
+            try:
+                uuid.UUID(note_uuid)
+            except ValueError:
+                print "Bad UUID", note_uuid, "in", path
+                raise
+            origin_file = unicode(path)
+            if note_uuid in uuids:
+                raise Exception("Duplicated UID:" + str(note_uuid))
+            uuids.add(note_uuid)
+            # print (origin_file, note_uuid)
+            # (front) (back) (add-reverse) (origin-file) (note_uuid) (git-revision)
+            if 'include_reverse' in note_row:
+                include_reverse = note_row['include_reverse']
+            else:
+                include_reverse = False
+
+            note_topic = topic
+            if 'topic' in note_row:
+                note_topic = note_row['topic']
+
+            if 'front' not in note_row:
+                raise Exception('No front: ' + note_uuid)
+
+            front = unicode(note_row['front'])
+            back = unicode(note_row['back'])
+
+            if ('markdown' not in note_row) or (note_row['markdown']):
+                front = markdown.markdown(unicode(note_row['front']))
+                back = markdown.markdown(unicode(note_row['back']))
+
+            my_notes.append(Note(
+                origin_file = origin_file,
+                uuid = note_uuid,
+                include_reverse = include_reverse,
+                topic = note_topic,
+                front = front,
+                back = back,
+                git_revision = git_revision,
+                deck = deck
+            ))
+    return my_notes
--- pre-commit.py
+++ pre-commit.py
@@ -1,41 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
-import sys
-import os
-import fnmatch
-import yaml
-
-yaml_files = []
-for root, dirnames, filenames in os.walk('notes'):
-    for filename in fnmatch.filter(filenames, '*.yaml'):
-        yaml_files.append(os.path.join(root, filename))
-
-uuids = set()
-for path in yaml_files:
-    # print(path)
-    with open(path) as yf:
-        try:
-            data = yaml.load(yf)
-        except:
-            print('Invalid YAML file:', path)
-            raise
-
-        for note in data['notes']:
-            if not note:
-                print(path)
-                print("no note")
-                sys.exit(1)
-            if 'uuid' not in note:
-                print(note)
-                print('No UUID')
-                sys.exit(1)
-            if 'front' not in note or 'back' not in note:
-                print(note)
-                print('Missing front or back')
-                sys.exit(1)
-            uuid = note['uuid']
-            if uuid in uuids:
-                print('Duplicated UUID:', uuid)
-                sys.exit(1)
-            else:
-                uuids.add(uuid)
+import cards
+cards.load_all_notes()
